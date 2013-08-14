d17dfdfafbf861e15542a4a94ef36a9a061daab1
wetoolaguer <wenceslaoolaguer@gmail.com>
2013-08-14 14:42:15 +0800

    Major architecture change. [f:4h]
    - Version 1.0.0
    - HTTP methods GET, POST, PUT and DEL moved to
    RestClient
    - New method for RestClient, constructURI
    - New ResourceClient methods findBy, create, update
    and deleteBy
    - ResourceClient methods now accept reqObj
    instead of strings
    - Routes now support path overriding:
    for route { get:'/users/:id' } when accessed in the
    findBy method with reqObj parameter { id : 1 },
    the resulting request will have a path /users/1
    - changePath method removed since the same effect
    can be achieved with route overriding
    - resetPath method removed since we dont change path
    anymore
    - Namespace Factory removed, same function achieved
    with RestClient property assignment.

--- test/rest_client.js
+++ test/rest_client.js
@@ -25,47 +25,21 @@ describe("RestClient", function () {
         assert.equal(typeof restClient.users, 'object');
     });
 
-    it("should change basePath", function (done) {
-
-        async.waterfall([
-            function (callback) {
-                restClient.changePath ("/newpath");
-                restClient.users.get(function(err, resp, body) {
-                    assert.equal(resp.body, "This is a new path for Users page.");
-                    callback();
-                });
-            },
-            function (callback) {
-                restClient.changePath ("");
-                restClient.users.get(function(err, resp, body) {
-                    assert.equal(resp.body, "This is Users page.");
-                    callback();
-                });
-            }
-        ], function (err) {
-            done();
-        });
-    });
-
     describe("Generated Namespace", function() {
-        it("should have get method", function () {
-            assert.equal(typeof restClient.users.get, 'function');
-        });
-
-        it("should have post method", function () {
-            assert.equal(typeof restClient.users.post, 'function');
-        });
-
-        it("should have put method", function () {
-            assert.equal(typeof restClient.users.put, 'function');
-        });
-
-        it("should have del method", function () {
-            assert.equal(typeof restClient.users.del, 'function');
-        });
-
         it("should have findBy method", function () {
             assert.equal(typeof restClient.users.findBy, 'function');
+        });
+
+        it("should have create method", function () {
+            assert.equal(typeof restClient.users.createBy, 'function');
+        });
+
+        it("should have update method", function () {
+            assert.equal(typeof restClient.users.updateBy, 'function');
+        });
+
+        it("should have delete method", function () {
+            assert.equal(typeof restClient.users.deleteBy, 'function');
         });
     });
 });
